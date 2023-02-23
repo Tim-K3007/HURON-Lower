62c3852c87f5a7f9a58b0834d07e398bf5182f9a
Tim-K3007 <oitsplazma444@gmail.com>
2023-02-23 17:20:58 -0500

    whoops I messed up my left and right

--- robot_control/main.py
+++ robot_control/main.py
@@ -1,25 +1,25 @@
 from motorCon import motorCon
 import time
 
-right_roll = motorCon("can0", 0x1)
-right_knee = motorCon("can0", 0x0)
+left_roll = motorCon("can0", 0x1)
+left_knee = motorCon("can0", 0x0)
 time.sleep(15)
 
 # for x in range(10):
 #     if x % 2 == 0:
-#         right_knee.move_motor(10, 0, 0) 
-#         right_roll.move_motor(5, 0, 0)
+#         left_knee.move_motor(10, 0, 0) 
+#         left_roll.move_motor(5, 0, 0)
 #     else:
-#         right_knee.move_motor(0, 0, 0)
-#         right_roll.move_motor(-5, 0, 0)
+#         left_knee.move_motor(0, 0, 0)
+#         left_roll.move_motor(-5, 0, 0)
 
-right_knee.move_motor(10, 0, 0) 
-right_roll.move_motor(5, 0, 0)
-right_knee.move_motor(0, 0, 0)
-right_roll.move_motor(-5, 0, 0)
+left_knee.move_motor(10, 0, 0) 
+left_roll.move_motor(5, 0, 0)
+left_knee.move_motor(0, 0, 0)
+left_roll.move_motor(-5, 0, 0)
 
-right_knee.move_motor(0, 0, 0)
-right_roll.move_motor(0, 0, 0)
+left_knee.move_motor(0, 0, 0)
+left_roll.move_motor(0, 0, 0)
 
-right_roll.kill_motor()
-right_knee.kill_motor()
+left_roll.kill_motor()
+left_knee.kill_motor()
