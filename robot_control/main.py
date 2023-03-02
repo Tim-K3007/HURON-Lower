16f9c56b04713989c36da1b2a34397a63feb2201
Tim-K3007 <oitsplazma444@gmail.com>
2023-03-02 17:21:19 -0500

    updating pi for test

--- robot_control/main.py
+++ robot_control/main.py
@@ -40,6 +40,7 @@ def move_legs(left_hip_yaw, left_hip_pitch, left_hip_roll, left_knee,
     while not done:
         i += 1
         done = left_roll_motor.check_if_there()# and left_knee_motor.check_if_there() and right_roll_motor.check_if_there() and right_knee_motor.check_if_there()
+        print("done")
         if not done and i == 10000:
             print("fuck")
             print(left_roll_motor.check_if_there())
